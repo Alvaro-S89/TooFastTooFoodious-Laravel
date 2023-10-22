81ed16fe55107b77d18cf7b0fe04915dcca59b32
Alvaro <alvsr89@gmail.com>
2023-10-22 21:39:02 +0200

    feat:authentication, registration and logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -26,10 +28,30 @@ class AuthController extends Controller
         ];
     }
     
-    public function login(Request $request){
+    public function login(LoginRequest $request){
+        $data = $request->validated();
 
+        //login vcalidation
+        if(!Auth::attempt($data)){
+            return response([
+                'errors' => ['Email o contraseña incorrectas']
+            ], 422);
+        }
+
+        //user auth
+        $user = Auth::user();
+        return [
+            'token' => $user->createToken('token')->plainTextToken,
+            'user' => $user
+        ];
     }
-    public function logout(Request $request){
 
+    public function logout(Request $request){
+        $user = $request-> user();
+        $user->currentAccessToken()->delete();
+
+        return [
+            'user' => null
+        ];
     }
 }
