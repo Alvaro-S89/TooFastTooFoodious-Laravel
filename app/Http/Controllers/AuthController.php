968fbc4e5563ce3ffa742f76620e256e9d9ca1bc
Alvaro <alvsr89@gmail.com>
2023-10-23 21:33:28 +0200

    feat: model, migrations and controller for orders

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+// use Laravel\Sanctum\PersonalAccessToken;
 
 class AuthController extends Controller
 {
---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+
+        $order = new Order;
+        $order -> user_id = Auth::user()->id;
+        $order -> total = $request->total;
+        $order -> save();
+        return [
+            'message'=>'realizando pedido'
+        ];
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Order $order)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Order $order)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Order $order)
+    {
+        //
+    }
+}
